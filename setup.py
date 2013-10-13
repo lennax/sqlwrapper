60cfaf38e9a41097f0214ff25be1c25e74d56fae
Lenna Peterson <arklenna@gmail.com>
2013-10-13 15:50:00 -0400

    Convert to package

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='DBConnection',
+    version='0.1.0',
+    author='Lenna X. Peterson',
+    author_email='lxpeters@purdue.edu',
+    packages=['db'],
+    scripts=[
+    ],
+    url='',
+    license='LICENSE.txt',
+    description='SQL wrapper',
+    long_description=open('README.txt').read(),
+)
